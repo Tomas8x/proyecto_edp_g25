27721483a6bfd85c3f7688a78f9d1b4706f165d3
Tomas Avecilla <tomas8avecilla@gmail.com>
2023-06-20 18:32:29 -0300

    Actualizacion: se corrigieron algunos errores en el script. La funcion obtener_nombres esta terminada y comentada

--- generar.sh
+++ generar.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-<<ENUNCIADO generar.sh: Genera imágenes utilizando algún servicios web. Se debe
-poder indicar por argumento cuantas imagenes generar y se deben asignar
-nombres de archivo al azar de una lista de nombres de personas. Tener
-en cuenta que al descargar de una página conviene usar un sleep entre
-descarga y descarga para no saturar el servicio y evitar problemas. Luego
-se deben comprimir las imágenes, y generar un archivo con su suma de
-verificación. ENUNCIADO>>
 #Espacio para documentar
 
 regex=.^[1-9][0-9]*$
@@ -16,10 +9,14 @@ url=https://source.unsplash.com/random/900%C3%97700/?person
 
 }
 
-function obtener_nombres { # Funcion que retorna un nombre aleatorio desde un dataset .csv descargado de un url.
-url=https://raw.githubusercontent.com/fernandezpablo85/name_suggestions/master/assets/dict.csv
-curl -O "dict.csv" "$url"
-awk 
-}
-# Mañana sigo... habria que buscar en el archivo descargado dict.csv un nombre unico y aleatorio de la lista...
+function obtener_nombres { # Funcion que da un nombre aleatorio desde un dataset .csv descargado de un url.
+  url="https://raw.githubusercontent.com/fernandezpablo85/name_suggestions/master/assets/dict.csv"
+  curl -O "$url"
+  NOMBRE=$(awk -F ',' '{print $1}' dict.csv | shuf -n 1) #  awk procesa el texto y extrae los primeros campos de cada línea, shuf filtra y selecciona aleatoriamente una línea de la salida de awk.
 
+echo "El nombre obtenido es: $NOMBRE_OBTENIDO"
+  echo "$NOMBRE"
+  return 0
+}
+NOMBRE_OBTENIDO=$(obtener_nombres)
+echo "El nombre obtenido es: $NOMBRE_OBTENIDO"
