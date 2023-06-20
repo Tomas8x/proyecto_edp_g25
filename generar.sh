0b69669b9bded726eec8385c6408ac8d43e29826
Tomas Avecilla <tomas8avecilla@gmail.com>
2023-06-20 20:37:48 -0300

    Actualizacion y problema: La generacion de imagenes esta hecha y faltaria agregarle al script la compresion y generar el archivo con la suma de verificaciones. El problema que tengo yo es que las imagenes se bajan bien pero no me deja abrirlas por un error de formato. La busqueda en la url funciona bien asique me parece que es un problema de mi pc, no se, fijense si a ustedes les deja.

--- generar.sh
+++ generar.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-#Espacio para documentar
+# Este script toma un argumento numerico que marca la cantidad de imagenes que queremos descargar de una pagina web, les da un nombre valido las comprime y genera un archivo con su suma de verificación.
 
-regex=.^[1-9][0-9]*$
 function obtener_imagen { # Funcion que retorna una imagen aleatoria desde una pagina web.
-
-url=https://source.unsplash.com/random/900%C3%97700/?person
-
+  url="https://source.unsplash.com/random/900%C3%97700/?person."
+  IMAGEN="imagen.jpg"
+  curl -o "$IMAGEN" "$url"
+  sleep 2
+  echo "$IMAGEN"
 }
 
 function obtener_nombres { # Funcion que da un nombre aleatorio desde un dataset .csv descargado de un url.
   url="https://raw.githubusercontent.com/fernandezpablo85/name_suggestions/master/assets/dict.csv"
   curl -O "$url"
-  NOMBRE=$(awk -F ',' '{print $1}' dict.csv | shuf -n 1) #  awk procesa el texto y extrae los primeros campos de cada línea, shuf filtra y selecciona aleatoriamente una línea de la salida de awk.
-
-echo "El nombre obtenido es: $NOMBRE_OBTENIDO"
+  NOMBRE=$(awk -F ",| " '{print $1}' dict.csv | shuf -n 1) # awk procesa el texto y extrae los primeros campos de cada línea, shuf filtra y selecciona aleatoriamente una línea de la salida de awk.
   echo "$NOMBRE"
-  return 0
 }
-NOMBRE_OBTENIDO=$(obtener_nombres)
-echo "El nombre obtenido es: $NOMBRE_OBTENIDO"
+
+echo "Ingrese la cantidad de imágenes que desea generar: "
+read ARGS
+regex='^[1-9][0-9]*$' # Expresion regular para que el ingreso sea un numero mayor a 0.
+if [[ $ARGS =~ $regex ]]; then
+  for ((i=1; i<=ARGS; i++)); do # Valor inicial de la variable i=1. El bucle se ejecutará mientras i<=ARGS. Después de cada iteración, la variable i se incrementa en 1 mediante i++.
+    IMAGEN_GENERADA=$(obtener_imagen)
+    NOMBRE_GENERADO=$(obtener_nombres)
+    mv "$IMAGEN_GENERADA" "${NOMBRE_GENERADO}_${i}.jpg" # Esta sintaxis hace que los nombres de cada imagen sean unicos agregando al nombre el numero de iteracion por el que va el bucle for.
+  done
+fi
