2ac8bf55fc71dd569669a9b22e1fa6ec7a8152ec
Tomas Avecilla <tomas8avecilla@gmail.com>
2023-06-22 01:14:57 -0300

    Terminado terminado el script generar.sh y comentado.

--- generar.sh
+++ generar.sh
@@ -5,25 +5,36 @@
 function obtener_imagen { # Funcion que retorna una imagen aleatoria desde una pagina web.
   url="https://source.unsplash.com/random/900%C3%97700/?person."
   IMAGEN="imagen.jpg"
-  curl -o "$IMAGEN" "$url"
+  wget -O "$IMAGEN" "$url"
   sleep 2
   echo "$IMAGEN"
 }
 
 function obtener_nombres { # Funcion que da un nombre aleatorio desde un dataset .csv descargado de un url.
   url="https://raw.githubusercontent.com/fernandezpablo85/name_suggestions/master/assets/dict.csv"
-  curl -O "$url"
-  NOMBRE=$(awk -F ",| " '{print $1}' dict.csv | shuf -n 1) # awk procesa el texto y extrae los primeros campos de cada línea, shuf filtra y selecciona aleatoriamente una línea de la salida de awk.
+  wget -O "NOMBRES.CSV" "$url"
+  NOMBRE=$(awk -F ",| " '{print $1}' NOMBRES.CSV | sed -n "${RANDOM}p") # awk procesa el texto y extrae los primeros campos de cada línea, sed -n permite que se imprima el numero generado por RANDOM
   echo "$NOMBRE"
 }
 
+function comprimir { # Funcion que toma un directorio con imagenes y las comprime
+  tar zcvf imagenes.tar.gz $1
+}
+
+
+# Aca comienza el programa
 echo "Ingrese la cantidad de imágenes que desea generar: "
 read ARGS
 regex='^[1-9][0-9]*$' # Expresion regular para que el ingreso sea un numero mayor a 0.
 if [[ $ARGS =~ $regex ]]; then
-  for ((i=1; i<=ARGS; i++)); do # Valor inicial de la variable i=1. El bucle se ejecutará mientras i<=ARGS. Después de cada iteración, la variable i se incrementa en 1 mediante i++.
-    IMAGEN_GENERADA=$(obtener_imagen)
-    NOMBRE_GENERADO=$(obtener_nombres)
-    mv "$IMAGEN_GENERADA" "${NOMBRE_GENERADO}_${i}.jpg" # Esta sintaxis hace que los nombres de cada imagen sean unicos agregando al nombre el numero de iteracion por el que va el bucle for.
-  done
+  mkdir imagenes_generadas
+    for ((i=1; i<=ARGS; i++)); do # Valor inicial de la variable i=1. El bucle se ejecutará mientras i<=ARGS. Después de cada iteración, la variable i se incrementa en 1 mediante i++.
+      IMAGEN_GENERADA=$(obtener_imagen)
+      NOMBRE_GENERADO=$(obtener_nombres)
+      mv "$IMAGEN_GENERADA" "${NOMBRE_GENERADO}_${i}.jpg" # Esta sintaxis hace que los nombres de cada imagen sean unicos agregando al nombre el numero de iteracion por el que va el bucle for.
+      mv "${NOMBRE_GENERADO}_${i}.jpg" imagenes_generadas
+    done
+  comprimir imagenes_generadas | md5sum > suma_de_verificaciones.txt # Comprime el directorio con imagenes y crea un .txt con la suma de verificaciones de la compresion.
+else
+  echo "Ingrese un numero mayor a 0"
 fi
